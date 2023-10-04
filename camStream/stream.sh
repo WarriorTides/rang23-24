2e2dfeb218942c19df565d40f25a93d5350c85b7
amaheshwari01 <acoding2007@gmail.com>
2023-10-03 22:17:57 -0700

    add streaming scripts

---
+++ camStream/stream.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Script to be run at EVERY boot
+
+if [[ "$(id -u)" != 0 ]]
+  then echo "Please run as root"
+  exit
+fi
+
+raspi-config nonint do_legacy 0
+
+export ROTATION=0
+export WIDTH=1280
+export HEIGHT=720
+export FPS=2
+
+raspivid -n -cd MJPEG -awb auto -ifx none -b 25000000 -br 60 -t 0 -rot ${ROTATION} -w ${WIDTH} -h ${HEIGHT} -fps ${FPS} -o - | ncat -lkv4 5000
