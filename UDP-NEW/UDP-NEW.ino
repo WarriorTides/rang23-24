75cd68a945c9ca82cf08909ad36d195717e707fb
amaheshwari01 <acoding2007@gmail.com>
2023-11-04 21:25:37 -0700

    Commented out code for remote IP and port printing, added comments for packet sending and connection restarting

--- UDP-NEW/UDP-NEW.ino
+++ UDP-NEW/UDP-NEW.ino
@@ -42,45 +42,45 @@ void loop()
 
         Serial.println(("'"));
 
-        int success;
-        do
-        {
+        //     int success;
+        //     do
+        //     {
 
-            Serial.print(("remote ip: "));
+        //         Serial.print(("remote ip: "));
 
-            Serial.println(udp.remoteIP());
+        //         Serial.println(udp.remoteIP());
 
-            Serial.print(("remote port: "));
-            Serial.println(udp.remotePort());
+        //         Serial.print(("remote port: "));
+        //         Serial.println(udp.remotePort());
 
-            // send new packet back to ip/port of client. This also
-            // configures the current connection to ignore packets from
-            // other clients!
-            success = udp.beginPacket(udp.remoteIP(), udp.remotePort());
+        //         // send new packet back to ip/port of client. This also
+        //         // configures the current connection to ignore packets from
+        //         // other clients!
+        //         success = udp.beginPacket(udp.remoteIP(), udp.remotePort());
 
-            Serial.print(("beginPacket: "));
-            Serial.println(success ? "success" : "failed");
+        //         Serial.print(("beginPacket: "));
+        //         Serial.println(success ? "success" : "failed");
 
-            // beginPacket fails if remote ethaddr is unknown. In this case an
-            // arp-request is send out first and beginPacket succeeds as soon
-            // the arp-response is received.
-        } while (!success);
+        //         // beginPacket fails if remote ethaddr is unknown. In this case an
+        //         // arp-request is send out first and beginPacket succeeds as soon
+        //         // the arp-response is received.
+        //     } while (!success);
 
-        success = udp.println(F("hello world from arduino"));
+        //     success = udp.println(F("hello world from arduino"));
 
-        Serial.print(("bytes written: "));
-        Serial.println(success);
+        //     Serial.print(("bytes written: "));
+        //     Serial.println(success);
 
-        success = udp.endPacket();
+        //     success = udp.endPacket();
 
-        Serial.print(("endPacket: "));
-        Serial.println(success ? "success" : "failed");
+        //     Serial.print(("endPacket: "));
+        //     Serial.println(success ? "success" : "failed");
 
-        udp.stop();
-        // restart with new connection to receive packets from other clients
-        success = udp.begin(5000);
+        //     udp.stop();
+        //     // restart with new connection to receive packets from other clients
+        //     success = udp.begin(5000);
 
-        Serial.print(("restart connection: "));
-        Serial.println(success ? "success" : "failed");
+        //     Serial.print(("restart connection: "));
+        //     Serial.println(success ? "success" : "failed");
     }
 }
