ee8d64be0a919d9fd7c6d0f6b25ea35649aac9b0
Silverleaf Yen <80929478+silverleaf321@users.noreply.github.com>
2023-11-15 15:20:00 -0800

    imu test code

---
+++ imu_test.ino
@@ -0,0 +1,32 @@
+#include <Adafruit_BNO055.h>
+
+Adafruit_BNO055 bno = Adafruit_BNO055();
+
+void setup() {
+  Serial.begin(9600);
+
+  // Initialize the BNO08x
+  if (!bno.begin()) {
+    Serial.println("Failed to initialize BNO08x");
+    while (1);
+  }
+
+  Serial.println("BNO08x initialized successfully!");
+}
+
+void loop() {
+  // Get the current orientation
+  sensors_event_t event;
+  bno.getEvent(&event);
+
+  // Print the current orientation to the serial monitor
+  Serial.print("Orientation: ");
+  Serial.print(event.orientation.x);
+  Serial.print(", ");
+  Serial.print(event.orientation.y);
+  Serial.print(", ");
+  Serial.println(event.orientation.z);
+
+  // Delay for 100 milliseconds
+  delay(100);
+}
