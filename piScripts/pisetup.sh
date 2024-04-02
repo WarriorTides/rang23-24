585a6e1deeb589396843c377d3cafbf04d6fb7f3
amaheshwari01 <acoding2007@gmail.com>
2024-04-02 16:20:07 -0700

    clean up pisetup script

--- piScripts/pisetup.sh
+++ piScripts/pisetup.sh
@@ -20,12 +20,10 @@ rpi-update -y
 
 # Checks if ncat and nmap are installed and then installs them if they are not
 # Needed for the raspivid function to stream the camera
-dpkg -l | grep git || apt install git -y
+# dpkg -l | grep git || apt install git -y
 dpkg -l | grep ncat || apt install ncat -y
 dpkg -l | grep nmap || apt install nmap -y
 curl -d "Packages are are installed" ntfy.aayanmaheshwari.com/test
-
-git clone https://github.com/WarriorTides/rang23-24.git
 
 
 # for arg in "$@"
@@ -57,16 +55,16 @@ echo "gpu_mem=128" >> /boot/config.txt
 echo "disable_camera_led=1" >> /boot/config.txt
 
 
-crontab -l > mycron
+#crontab -l > mycron
 #echo new cron into cron file
 
-echo "alias camrun='sudo bash /home/pi/rang23-24/piScripts/picam.sh'" >> ~/.bashrc
-echo "@reboot bash /home/pi/rang23-24/piScripts/picam.sh" >> mycron
+echo "alias camrun=sudo bash /home/pi/rang23-24/piScripts/picam.sh'" >> ~/.bashrc
+#echo "@reboot bash /home/pi/rang23-24/piScripts/picam.sh" >> mycron
 
 #echo "@reboot bash $(SCRIPT_DIR)/usbip.sh" >> mycron
 
-crontab mycron
-rm mycron
+#crontab mycron
+#rm mycron
 
 curl -d "PI IS SERTUP NOW AND IAM REBOOTING:" ntfy.aayanmaheshwari.com/test
 
