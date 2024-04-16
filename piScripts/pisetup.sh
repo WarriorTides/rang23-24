5b3bc6681dda531380816ab71fca01c3544bd296
amaheshwari01 <acoding2007@gmail.com>
2024-04-16 13:03:52 -0700

    update setpu scripts

--- piScripts/pisetup.sh
+++ piScripts/pisetup.sh
@@ -4,8 +4,10 @@ if [[ "$(id -u)" != 0 ]]
   exit
 fi
 
+ip route replace default via 10.78.0.1 dev wlan0 
+vcgencmd get_camera
 
-
+sleep 5
 # Pings google. if google breaks, we don't have access to start a new camera, sorry. Needs the internet and
 # this was the best way I could think of. If you have a better way to for it
 # TODO
@@ -16,14 +18,20 @@ fi
 
 # Makes sure the PI is up to date. This could cause things to break in the future
 apt update -y
+# curl -d "apt updated" ntfy.aayanmaheshwari.com/test
+echo "apt updated"
 rpi-update -y
-
+# curl -d "rpi updated" ntfy.aayanmaheshwari.com/test
+echo "rpi updated"
 # Checks if ncat and nmap are installed and then installs them if they are not
 # Needed for the raspivid function to stream the camera
 # dpkg -l | grep git || apt install git -y
-dpkg -l | grep ncat || apt install ncat -y
-dpkg -l | grep nmap || apt install nmap -y
-curl -d "Packages are are installed" ntfy.aayanmaheshwari.com/test
+apt install git ncat nmap -y
+
+git clone https://github.com/WarriorTides/rang23-24.git 
+
+echo "Packages are installed ang git cloned"
+# curl -d "Packages are are installed" ntfy.aayanmaheshwari.com/test
 
 
 
@@ -35,13 +43,18 @@ curl -d "Packages are are installed" ntfy.aayanmaheshwari.com/test
 
 # enable cameras
 raspi-config nonint do_legacy 0
+# curl -d "camera enabled" ntfy.aayanmaheshwari.com/test
 
 # give more memory
-sudo echo -e "$(sed '/gpu_mem/d' /boot/config.txt)" > /boot/config.txt
-sudo echo "gpu_mem=128" >> /boot/config.txt
+echo -e "$(sed '/gpu_mem/d' /boot/config.txt)" > /boot/config.txt
+
+
+
+echo "gpu_mem=128" >> /boot/config.txt
+# curl -d "gpu_mem=128" ntfy.aayanmaheshwari.com/test
 
 # turn off the red light. if you leave it on, it reflects off the glass
-sudo echo "disable_camera_led=1" >> /boot/config.txt
+echo "disable_camera_led=1" >> /boot/config.txt
 
 
 #crontab -l > mycron
@@ -53,6 +66,7 @@ echo "alias camrun='sudo bash /home/pi/rang23-24/piScripts/picam.sh'" >> ~/.bash
 #echo "@reboot bash $(SCRIPT_DIR)/usbip.sh" >> mycron
 
 #crontab mycron
-#rm mycron
 
-reboot now
+# curl -d "camera setup complete" ntfy.aayanmaheshwari.com/test
+# reboot now
+echo "Setup is Complete Please Reboot!!"
